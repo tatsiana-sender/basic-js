3ad66a08d7749a18538980eee743c809b3c385ef
Tatsiana <49600781+tatsiana-sender@users.noreply.github.com>
2020-03-20 22:07:55 +0300

    add decision task dream team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(/*MATRIX*/) {
   throw 'Not implemented';
   // remove line with error and write your code here
+  // const arr = Array.flat().map(function(item) {
+  //           return item.charCodeAt();
+  //       });
+  //       let cats = [];
+  //       for (let i = 0; i < arr.length; i++) {
+  //           if (i == 94 && (i+1) == 94) {
+  //               cats.push(i);
+  //           }
+  //       }
+  //       return cats.length;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (members instanceof Array) {
+        var names = members.filter(function(elem) {
+            if (typeof elem == "string") {
+                return elem;
+            };
+        });
+        var letters = names.map(function(string) {
+            var letter = string.trim();
+            return letter[0].toUpperCase();
+        });
+        letters.sort();
+        var result = letters.join('');
+        return result;
+    } else {
+        return false;
+    };
 };
